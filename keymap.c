8d00cdb14878d7e9735925e31f844c9919b7c0b5
Heesu Han <99hshan@gmail.com>
2024-04-01 20:13:44 +0900

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -8,13 +8,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                            LALT_T(KC_DEL), LT(FUN_NUM, KC_BSPC), KC_LGUI,        KC_ENT,  LT(FUN_NUM, KC_SPC),  LT(NAV, KC_RALT)
+                                   KC_LALT, MO(FUN_NUM), KC_LGUI,                   RALT_T(KC_ENT), LT(FUN_NUM, KC_SPC),  MO(NAV)
     ),
     [FUN_NUM] = LAYOUT_split_3x6_3(
        	KC_ESC,  KC_GRV,  KC_F7,   KC_F8,   KC_F9,   KC_F12,                               KC_BSPC, KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC,
         _______, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                               KC_DEL,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_EQL,
         _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                               KC_0,    KC_1,    KC_2,    KC_3,    _______, _______,
-                                            _______, KC_BSPC, _______,            _______, KC_SPC,  KC_INS
+                                            _______, _______, _______,            KC_ENT,  KC_SPC,  KC_INS
     ),
     [NAV] = LAYOUT_split_3x6_3(
         KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_BSPC, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_UP,   KC_RGHT, KC_PGUP,
