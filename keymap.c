1f89d7f5558e0c0fa053bb0c16d1c54125d9bf54
Heesu Han <99hshan@gmail.com>
2024-05-29 23:27:16 +0900

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -2,18 +2,18 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include QMK_KEYBOARD_H
-enum layers {BASE, FUN_NUM, NAV};
+enum layers {BASE, NUM, NAV, FUN};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                        KC_LALT, MO(FUN_NUM), KC_LGUI,            KC_ENT,  LT(FUN_NUM, KC_SPC),  MO(NAV)
+                                        KC_LALT, MO(NUM), KC_LGUI,            KC_ENT,  LT(FUN_NUM, KC_SPC),  MO(NAV)
     ),
-    [FUN_NUM] = LAYOUT_split_3x6_3(
-       	KC_ESC,  KC_GRV,  KC_F7,   KC_F8,   KC_F9,   KC_F12,                               KC_BSPC, KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC,
-        _______, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                               KC_DEL,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_EQL,
-        _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                               KC_0,    KC_1,    KC_2,    KC_3,    _______, _______,
+    [NUM] = LAYOUT_split_3x6_3(
+       	KC_ESC,  KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               KC_BSPC, KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               KC_DEL,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_EQL,
+        _______, MO(FUN), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               KC_0,    KC_1,    KC_2,    KC_3,    _______, _______,
                                             _______, _______, _______,            _______, KC_SPC,  KC_RALT
     ),
     [NAV] = LAYOUT_split_3x6_3(
@@ -22,5 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_INS,  KC_END,  KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, LGUI(KC_D), _______,
                                             _______, _______, _______,            XXXXXXX, XXXXXXX, XXXXXXX
     )
+    [FUN] = LAYOUT_split_3x6_3(
+       	XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F12,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                                            _______, _______, _______,            XXXXXXX, XXXXXXX, XXXXXXX
+    ),
 };
 
